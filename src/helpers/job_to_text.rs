d9126538fa872925ede3374f5a15cd0b37ed9c25
vicsnet <adesanmiadebisi@gmail.com>
2026-04-04 11:02:54 +0100

    feat: helpers:  job_to_text added

---
+++ src/helpers/job_to_text.rs
@@ -0,0 +1,11 @@
+use crate::controllers::handlers::api_calls::Job;
+
+pub fn job_to_text(job: Job) -> String {
+    format!(
+        "{}. {}. {}. {}",
+        job.title,
+        job.organisation,
+        job.location,
+        job.description
+    )
+}
---
+++ src/helpers/mod.rs
@@ -0,0 +1,1 @@
+pub mod job_to_text;
