d141cddf1afdb85cb536d1eaa5aeff0eb5971c28
vicsnet <adesanmiadebisi@gmail.com>
2026-04-06 14:48:46 +0100

    feat: update_user_state fn added

--- src/bot/my_bot.rs
+++ src/bot/my_bot.rs
@@ -1,16 +1,19 @@
 use teloxide::{ prelude::*, types::User };
 use dotenvy::dotenv;
-use crate::controllers::handlers::job_matching::match_cv_to_jobs;
+use crate::controllers::handlers::{
+    job_matching::match_cv_to_jobs,
+    users::get_user_by_telegram_id,
+};
 
 use sqlx::PgPool;
 use reqwest::Client;
+use crate::controllers::handlers::users::{ create_user, update_user_cv };
 
 pub async fn run_bot(pool: PgPool, client: Client) {
     dotenv().ok();
 
     let token = std::env::var("BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-let bot = Bot::new(token);
-    
+    let bot = Bot::new(token);
 
     teloxide::repl(bot, move |bot: Bot, msg: Message| {
         let pool = pool.clone();
@@ -18,11 +21,43 @@ let bot = Bot::new(token);
 
         async move {
             if let Some(text) = msg.text() {
+
+                let telegram_id = msg.chat.id.to_string();
+
+                create_user(&pool, &msg.chat.id.to_string()).await.unwrap_or_else(|e| {
+                    eprintln!("Error creating user: {}", e);
+                });
+                
                 if text == "/start" {
+
                     bot.send_message(msg.chat.id, "👋 Send me your resume").await?;
                     return respond(());
                 }
-                bot.send_message(msg.chat.id, "⏳ Processing your CV...").await?;
+
+                let existing_user = get_user_by_telegram_id(
+                    &pool,
+                    &msg.chat.id.to_string()
+                ).await.unwrap_or_else(|e| {
+                    eprintln!("Error fetching user: {}", e);
+                    None
+                });
+
+                if existing_user.is_none() {
+                    bot.send_message(msg.chat.id, "💾 Saving your CV...").await?;
+
+                    update_user_cv(
+                        &pool,
+                        &msg.chat.id.to_string(),
+                        text,
+                        &client
+                    ).await.unwrap_or_else(|e| {
+                        eprintln!("Error updating user CV: {}", e);
+                    });
+                    bot.send_message(msg.chat.id, "💾 Saving your CV...").await?;
+                    bot.send_message(msg.chat.id, "⏳ Processing your CV...").await?;
+                } else {
+                    bot.send_message(msg.chat.id, "⏳ Processing your CV...").await?;
+                }
 
                 let result = match_cv_to_jobs(text, &pool, &client).await;
 
--- src/controllers/handlers/users.rs
+++ src/controllers/handlers/users.rs
@@ -4,8 +4,8 @@ use reqwest::Client;
 use crate::controllers::embedding::text_to_vec::get_embeddings;
 
 
-
-struct User{
+#[derive(Debug)]
+pub struct User{
     pub id: i32,
     pub telegram_id: String,
     pub cv_text: String,
@@ -37,13 +37,45 @@ pub async fn update_user_cv(pool: &PgPool, telegram_id: &str, cv_text: &str, cli
         };
 
         sqlx::query(
-            "UPDATE users SET cv_text = $1, cv_embeddings = $2 WHERE telegram_id = $3"
+            "UPDATE users SET cv_text = $1, cv_embedding = $2 WHERE telegram_id = $3"
         )
         .bind(cv_text)
         .bind(&cv_embedding)
         .bind(telegram_id)
         .execute(pool)
         .await?;
+    // Placeholder for the actual implementation
+    Ok(())
+}
+
+
+pub async fn get_user_by_telegram_id(pool: &PgPool, telegram_id: &str) -> Result<Option<User>, sqlx::Error>{
+    let row = sqlx::query("SELECT id, telegram_id, cv_text, cv_embeddings, created_at FROM users WHERE telegram_id = $1")
+        .bind(telegram_id)
+        .fetch_optional(pool)
+        .await?;
+
+    if let Some(row) = row {
+        Ok(Some(User{
+            id: row.try_get("id")?,
+            telegram_id: row.try_get("telegram_id")?,
+            cv_text: row.try_get("cv_text")?,
+            cv_embeddings: row.try_get("cv_embeddings")?,
+            created_at: row.try_get("created_at")?,
+        }))
+    } else {
+        Ok(None)
+    }
+}
+
+pub async fn update_user_state(pool: &PgPool, telegram_id: &str, state: &str) -> Result<(), sqlx::Error>{
+    sqlx::query(
+        "UPDATE users SET state = $1 WHERE telegram_id = $2"
+    )
+    .bind(state)
+    .bind(telegram_id)
+    .execute(pool)
+    .await?;
     // Placeholder for the actual implementation
     Ok(())
 }
