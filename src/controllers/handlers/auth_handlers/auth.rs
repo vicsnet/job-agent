8c304b0163ba4bd78292f3b1d418cd80fc50b591
vicsnet <adesanmiadebisi@gmail.com>
2026-07-25 16:25:42 +0100

    feat: controllers : auth /register auth/login added

---
+++ src/controllers/handlers/auth_handlers/auth.rs
@@ -0,0 +1,24 @@
+use crate::controllers::login::register::{register_user, users_login, RegisterRequest, LoginRequest};
+use actix_web::{web, HttpResponse, post, Responder};
+use sqlx::PgPool;
+use serde_json::json;
+
+
+
+
+
+#[post("/register")]
+pub async fn register(pool: web::Data<PgPool>, body: web::Json<RegisterRequest>) -> impl Responder{
+    match register_user(pool.get_ref(), body.into_inner()).await{
+        Ok(token) => HttpResponse::Created().json(json!({ "token": token })),
+        Err(e) => HttpResponse::BadRequest().json(json!({ "error": e.to_string() })),
+    }
+}
+
+#[post("/login")]
+pub async fn login(pool: web::Data<PgPool>, body: web::Json<LoginRequest>) -> impl Responder{
+    match users_login(pool.get_ref(), body.into_inner()).await{
+        Ok(token) => HttpResponse::Ok().json(json!({ "token": token })),
+        Err(e) => HttpResponse::Unauthorized().json(json!({ "error": e.to_string() })),
+    }
+}
--- src/controllers/handlers/mod.rs
+++ src/controllers/handlers/mod.rs
@@ -2,3 +2,4 @@ pub mod api_calls;
 pub mod job_matching;
 pub mod users;
 pub mod open_ai;
+pub mod auth_handlers;
